ad4f0793cac86c1914827b3724c6b5efe76f4d45
John Nishinaga <jingoro@casa-z.org>
2013-04-24 01:04:38 -0700

    Migrate to Middleman

--- Gemfile
+++ Gemfile
@@ -1,33 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'nanoc', '~> 3.3.6'
-gem 'haml', '~> 3.1.6'
-gem 'sass', '~> 3.1.19'
-gem 'compass', '~> 0.12.1'
-gem 'compass-h5bp', '>= 0.0.5'
-gem 'nanoc-cachebuster', '>= 0.3.1'
-gem 'nanoc-image-compressor', '>= 0.1.1'
-gem 'nanoc-javascript-concatenator'
+gem 'middleman', '~>3.1.0.beta.1'
+gem 'middleman-deploy'
 
-gem 'activesupport'
-gem 'kramdown'
-gem 'uglifier'
-gem 'multi_json', '~> 1.2.0' # needed for uglifier v1.2.4
-
-# needed for tasks
-gem 'rake'
-
-# needed to run nanoc autocompile
-gem 'rack'
-gem 'mime-types'
-gem 'i18n'
-
-# needed to run nanoc view
-gem 'adsf'
-
-# needed to run nanoc deploy
-gem 'systemu'
-
-# needed to run validations
-gem 'w3c_validators'
-gem 'nokogiri'
+gem 'rb-fsevent', '~> 0.9.3' # OS X
---
+++ config.rb
@@ -0,0 +1,94 @@
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+activate :directory_indexes
+
+page "/404.html", :directory_index => false
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+helpers do
+
+  JOHN_LINKS = YAML.load_file(File.expand_path('../source/john/links.yaml', __FILE__))
+  def john_link(name)
+    i = JOHN_LINKS[name]
+    img = image_tag "john-links/#{i['image']}", alt: i['name']
+    link_to img, i['url'], :title => i['name']
+  end
+
+end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :cache_buster
+  activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.user = 'jingoro'
+  deploy.host = 'imaginary.yerk.org'
+  deploy.path = '/var/www/static/ejinjue/root'
+end
